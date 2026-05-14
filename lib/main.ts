e606be91ed567c56bdb7eb2f7dabdc7a58827737
Aaron <aaron@noreply.midgar.city>
2026-05-14 14:29:21 +0000

    Add vars

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,4 @@
 export * from './createJPLDashboardReducer';
 export * from './JPLDashboardReducer';
 export * from './nodes';
+export * from './vars';
---
+++ lib/vars/index.ts
@@ -0,0 +1,5 @@
+// import { var_ } from './var';
+
+export const vars = {
+  // var: var_, // TODO: type jpl package first
+};
